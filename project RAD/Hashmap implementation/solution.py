4dd079e7157925606212ef0dfa7cd9826ca23df9
kamalsundarsk <kamalsundarsk@gmail.com>
2025-10-15 23:21:19 +0530

    Update solution.py

--- project RAD/Hashmap implementation/solution.py
+++ project RAD/Hashmap implementation/solution.py
@@ -7,7 +7,6 @@ def two_sums(nums: list[int] ,target:int) -> list[int]:
             complementind = seen[complement]
             return [complementind,curind]
         seen[currnum]=curind
-        
 nums = [2,5,6,7,11,15]
 target=9
 print(f"two sum{nums} : {two_sums(nums,target)}")
